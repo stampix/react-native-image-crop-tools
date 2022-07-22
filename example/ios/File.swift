a7310824848e4ca686cb43d5672cb90546f5a862
Mouwrice <mauricevanwassenhove@gmail.com>
2022-07-22 12:20:48 +0200

    chore: remove exapmle

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  CropToolsExample
-//
-
-import Foundation
--- example/react-native.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  dependencies: {
-    'react-native-crop-tools': {
-      root: path.join(__dirname, '..'),
-    },
-  },
-};
